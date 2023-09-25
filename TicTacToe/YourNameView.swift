edb95389315f1d07ffe9f667890aaa8658ac2d3e
Youngju Lee <youngju.lee1995@gmail.com>
2023-09-25 11:46:46 +1000

    Refactor code. Remove unnecessary comments. Add edit name functionality.

---
+++ TicTacToe/YourNameView.swift
@@ -0,0 +1,18 @@
+//
+//  YourNameView.swift
+//  TicTacToe
+//
+//  Created by Young Ju Lee on 25/9/2023.
+//
+
+import SwiftUI
+
+struct YourNameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YourNameView()
+}
